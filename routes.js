78f85417f8175c7c8f5b54f46d1010a0eb66617e
Kal-MH <rkfalgus713@gmail.com>
2020-03-23 11:49:03 +0900

    seperate routes and connect url to url controller

---
+++ routes.js
@@ -0,0 +1,36 @@
+//global
+const HOME = "/";
+const SEARCH = "/search";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+
+const USERS = "/users";
+const USER_DETAIL = "/:id";
+const EDIT_PROFILE = "/:id/edit-profile";
+const CHANGE_PASSWARD = "/:id/change-password";
+
+const VIDEOS = "/videos";
+const UPLOAD = "/upload";
+const VIDEO_DETAIL = "/:id";
+const EDIT_VIDEO = "/:id/edit";
+const DELETE_VIDEO = "/:id/delete";
+
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassward: CHANGE_PASSWARD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEO_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO
+};
+
+export default routes;
