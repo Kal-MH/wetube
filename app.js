ddab5b755e846e3b7f85091e83bc1e3540310f18
Kal-MH <rkfalgus713@gmail.com>
2020-04-07 15:32:52 +0900

    Join and Login

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import express from "express";
 import globalRouter from "./routers/globalRouter";
 import helmet from "helmet";
@@ -9,7 +14,13 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import { localMiddleware } from "./middlewares";
 
+import "./passport";
+
+dotenv.config();
+
 const app = express();
+
+const CokieStore = MongoStore(session);
 
 app.set("view engine", "pug");
 
@@ -21,6 +32,17 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CokieStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localMiddleware);
 
